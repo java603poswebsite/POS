22827f28ebcee2049e714e3d0bb004cb8fc5c758
Neko <ngolben@gmail.com>
2019-05-01 19:43:38 -0500

    Testing file creation

--- POS/src/WriteFileTest.java
+++ POS/src/WriteFileTest.java
@@ -10,17 +10,11 @@ public class WriteFileTest {
 		Product pineapple = new Product("PineApple", 10, 1, 10, "Hawaii");
 		Product Strawberry = new Product("Strawberry", 10, 1, 5, "California");
 		Product CannedFood = new Product("CannedFood", 10, 1, 2.5, "Minnesota");
-		Receipt receipt = new Receipt(1, 1);
+		Receipt receipt = new Receipt(2, 2);
 		
 		receipt.addReceiptItem(pineapple);
 		receipt.addReceiptItem(Strawberry);
 		receipt.addReceiptItem(CannedFood);
-		//URL urlmango = Object.class.getClass().getResource("Mango.jpg");
-		//System.out.println("File Path = " + urlmango.getPath());
-		
-		//Path currentRelativePath = Paths.get(""); 
-		//String s = currentRelativePath.toAbsolutePath().toString(); 
-		//System.out.println("Current relative path is: " + s);
 		
 		WriteReceipt wr = new WriteReceipt();
 		wr.writeReceipt(receipt);
