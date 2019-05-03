4824caa033edb1f46db610ad1a82c41a121d0e11
lukeduchen <lukeduchen@gmail.com>
2019-05-02 19:45:48 -0500

    Update Calc.java
    Added a setDate function.

--- POS/src/Calc.java
+++ POS/src/Calc.java
@@ -1,7 +1,10 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class Calc {
-	private UserReceiptList allSales;
+	private UserReceiptList allSales; // Record of all sales (lists that include items, register, cashier, etc.)
 	
 	public Calc() {
 		allSales = new UserReceiptList();
@@ -9,6 +12,11 @@ public class Calc {
 	
 	public void addSale(List<String> items, List<Integer> quantities, List<Double> prices, int registerID, int cashierID) {
 		Receipt thisSale = new Receipt(registerID, cashierID);
+		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		thisSale.setDate(dateFormat.format(date));
+		
 		for(int i = 0; i < items.size(); i++) {
 			Product p = new Product(items.get(i), quantities.get(i), 0, prices.get(i), null);
 			thisSale.addReceiptItem(p);
