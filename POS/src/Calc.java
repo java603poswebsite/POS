3d3aaf6adb76bfc6a82407ad37e2a64131be1ed1
lukeduchen <lukeduchen@gmail.com>
2019-05-03 22:44:43 -0500

    Update Calc.java

--- POS/src/Calc.java
+++ POS/src/Calc.java
@@ -4,10 +4,13 @@ import java.util.Date;
 import java.util.List;
 
 public class Calc {
-	private UserReceiptList allSales; // Record of all sales (lists that include items, register, cashier, etc.)
+	private UserReceiptList allSales; 		// Record of all sales (lists that include items, register, cashier, etc.)
+	private InventoryList masterInventory; 	// Reference to the master inventory list.
 	
-	public Calc() {
+	// Inject the master inventory list to the local register.
+	public Calc(InventoryList invList) {
 		allSales = new UserReceiptList();
+		masterInventory = invList;
 	}
 	
 	public void addSale(List<String> items, List<Integer> quantities, List<Double> prices, int registerID, int cashierID) {
@@ -20,6 +23,9 @@ public class Calc {
 		for(int i = 0; i < items.size(); i++) {
 			Product p = new Product(items.get(i), quantities.get(i), 0, prices.get(i), null);
 			thisSale.addReceiptItem(p);
+			
+			// Update the master inventory.
+			masterInventory.removeProduct(p);
 		}
 		allSales.addReceipt(thisSale);
 	}
