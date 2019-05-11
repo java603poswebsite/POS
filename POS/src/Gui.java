d1999e9249703c6ff562126db14e8b25187a237c
Maria <mtorres01@hamline.edu>
2019-05-11 14:26:13 -0500

    Login update and report button update
    login changed to the center of the screen.
    
    Homewin now links to reports

--- POS/src/Gui.java
+++ POS/src/Gui.java
@@ -1,21 +1,24 @@
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JTextField;
 import java.awt.Font;
+import java.awt.Window;
 import java.awt.Color;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
 
 public class Gui  {
 
 	private JFrame frame;
+	private JPasswordField passwordField;
 	private JTextField textField;
-	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -48,21 +51,16 @@ public class Gui  {
 		frame.getContentPane().setBackground(Color.DARK_GRAY);
 		frame.getContentPane().setForeground(Color.BLACK);
 		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setLocationRelativeTo(null); 
 		
 		JLabel lblUsername = new JLabel("Username ");
 		lblUsername.setForeground(Color.WHITE);
 		lblUsername.setFont(new Font("Tahoma", Font.BOLD, 14));
 		
-		textField = new JTextField();
-		textField.setColumns(10);
-		
 		JLabel lblPassword = new JLabel("Password");
 		lblPassword.setForeground(Color.WHITE);
 		lblPassword.setFont(new Font("Tahoma", Font.BOLD, 14));
-		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
 		
 		JLabel lblSupermartLogin = new JLabel("SUPERMART LOGIN ");
 		lblSupermartLogin.setForeground(Color.WHITE);
@@ -74,27 +72,60 @@ public class Gui  {
 		btnLogin.setFont(new Font("Tahoma", Font.BOLD, 14));
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+	
+				String uname= textField.getText();
+				String pas= passwordField.getText();
+				
+				
+				
+				
+				
+				
+				if (uname.equals("name") && pas.equals("password"))
+						{ 
+				
+					
+					JOptionPane.showMessageDialog(frame, "you are sucessfully logged in");
+					
+					HomeWin home = new HomeWin ();
+					((HomeWin) home).setVisible(true);
+					
+					
+					}
+				
+				else 
+				
+				{
+					
+					JOptionPane.showMessageDialog(frame, "Invalid username or password");
+				}
 			}
 		});
+		
+		passwordField = new JPasswordField();
+		
+		textField = new JTextField();
+		textField.setColumns(10);
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.TRAILING)
+			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(120)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(lblPassword, GroupLayout.PREFERRED_SIZE, 69, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
 						.addGroup(groupLayout.createSequentialGroup()
 							.addComponent(lblUsername)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(139, Short.MAX_VALUE))
-				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+							.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblPassword, GroupLayout.PREFERRED_SIZE, 69, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(passwordField)))
+					.addContainerGap(56, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(176)
 					.addComponent(btnLogin)
-					.addContainerGap(193, Short.MAX_VALUE))
+					.addContainerGap(179, Short.MAX_VALUE))
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap(137, Short.MAX_VALUE)
 					.addComponent(lblSupermartLogin, GroupLayout.PREFERRED_SIZE, 287, GroupLayout.PREFERRED_SIZE)
@@ -112,12 +143,12 @@ public class Gui  {
 					.addGap(18)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblPassword)
-						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(42)
 					.addComponent(btnLogin)
-					.addContainerGap(54, Short.MAX_VALUE))
+					.addContainerGap(52, Short.MAX_VALUE))
 		);
 		frame.getContentPane().setLayout(groupLayout);
 	}
 }
-
+////////
